acce1354315604a1942b3b0e046c26773b9ffc29
Dylan-Thorp <Theinferno350@gmail.com>
2018-10-05 17:19:55 -0700

    Added Encoder to Drivetrain

--- src/org/usfirst/frc/team3255/robot/subsystems/Drivetrain.java
+++ src/org/usfirst/frc/team3255/robot/subsystems/Drivetrain.java
@@ -5,6 +5,7 @@ import org.usfirst.frc.team3255.robot.commands.DriveWithJoystick;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
@@ -16,11 +17,15 @@ public class Drivetrain extends Subsystem {
 	private WPI_TalonSRX rightTalon = null;
 	private DifferentialDrive differentialDrive = null;
 	
+	private Encoder encoder = null;
+	
     public Drivetrain() {
 		super();
 
 		leftTalon = new WPI_TalonSRX(1);
 		rightTalon = new WPI_TalonSRX(3);
+		
+		encoder = new Encoder(0,1);
 		
 		leftTalon.setNeutralMode(NeutralMode.Brake);
 		rightTalon.setNeutralMode(NeutralMode.Brake);
@@ -36,7 +41,19 @@ public class Drivetrain extends Subsystem {
     public void arcadeDrive(double moveSpeed, double rotateSpeed) {
     	differentialDrive.arcadeDrive(moveSpeed, rotateSpeed);
     }
+    
+    public double getEncoderCount() {
+    	return encoder.get();
+    }
+    
+    public double getEncoderDistance() {
+    	return encoder.get() * 2;
+    }
 
+    public void resetEncoder() {
+    	encoder.reset();
+    }
+    
 	public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
